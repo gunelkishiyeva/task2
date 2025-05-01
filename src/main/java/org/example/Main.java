f3e871498779dff58f779f30c3d1506e546f3d62
Gunel Kishiyeva <gunelkishiyeva11@gmail.com>
2025-05-01 09:51:40 +0400

    add

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,40 @@
+package org.example;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int number = 12;
+
+        switch (number) {
+            case 17:
+                System.out.println("number is big");
+                break;
+            case 10:
+                System.out.println("number is lesser than 10");
+                break;
+            case 700:
+                System.out.println("number is bigger than 100");
+                break;
+
+        }
+            String sweet = "candy";
+
+            switch (sweet){
+                case ("candy"):
+                    System.out.println("this is candy");
+                    break;
+                case ("chocolate"):
+                    System.out.println("this is chocolate");
+                    break;
+                case ("cake"):
+                    System.out.println("this is cake");
+                    break;
+            }
+
+
+
+        }
+    }
+
+
